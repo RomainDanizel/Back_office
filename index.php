398f4c95b09d37218953d7d2739dac95bcfe47ae
RomainDanizel <romain.danizel@hetic.net>
2020-10-06 23:07:34 +0200

    debug

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once("configs/index.php");
-require_once("model/bdd.php");
-require_once("model/artist.php");
-$bdd = new Database();
-$db = $bdd->connect();
-$artist = new Artist();
+//require_once("configs/index.php");
+//require_once("model/bdd.php");
+//require_once("model/artist.php");
+//$bdd = new Database();
+//$db = $bdd->connect();
+//$artist = new Artist();
 
 if (isset($_GET["p"])) {
     $page = $_GET["p"];
