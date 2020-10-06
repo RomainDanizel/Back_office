5d2fb299fdb84b98420dbae6542edae799e99b26
RomainDanizel <romain.danizel@hetic.net>
2020-10-06 23:10:52 +0200

    debug

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 require_once("configs/index.php");
-//require_once("model/bdd.php");
+require_once("model/bdd.php");
 require_once("model/artist.php");
-//$bdd = new Database();
+$bdd = new Database();
 //$db = $bdd->connect();
 //$artist = new Artist();
 
